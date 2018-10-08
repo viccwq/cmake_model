f2b9dafe9231ff7f7eabbbf783588fbd77485d12
viccwq <vic_cwq@163.com>
2018-10-08 23:10:23 +0800

    add trees

---
+++ modules/example/include/drawImg.h
@@ -0,0 +1,14 @@
+#ifndef DRAWIMG_H
+#define DRAWIMG_H
+class CDrawImg
+{
+public:
+    void draw();
+    void setFile(const string fileName);
+protected:
+private:
+};
+
+
+
+#endif
---
+++ modules/example/src/drawImg.cpp
@@ -0,0 +1,11 @@
+#include "drawImg.cpp"
+
+void CDrawImg::draw()
+{
+    return;
+}
+
+void setFile(const string fileName)
+{
+    return;
+}
