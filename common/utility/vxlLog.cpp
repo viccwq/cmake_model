7e90dd7f2d386dd7687a240849b113106d4660af
viccwq <vic_cwq@163.com>
2019-01-07 23:23:37 +0800

    add log, need to modify

---
+++ common/utility/vxlLog.cpp
@@ -0,0 +1,304 @@
+#include <iostream>
+#include <stdarg.h>
+
+#include "vxlLog.h"
+
+#ifdef _WIN32
+#define LOG_FILE_DIR  "C:/DebugLog"
+#else if defined __linux__
+#define LOG_FILE_DIR  "/DebugLog"
+#endif
+#define LOG_FILE_NAME "Codyy_VCA"
+#define LOG_FILE_EXT  ".txt"
+
+#define LOG_DEBUG_PRNT        0 // default 0, to control whether to print log debug info or not
+#define MAX_LOG_SIZE          (10 * 1024 * 1024)
+#define MAX_STR_SIZE_INLINE   (1024)
+
+FILE*         Logger::m_hLogFile  = NULL;
+VCA_LOG_LEVEL Logger::m_logLevel  = VCA_LOG_LEVEL_DEFAULT;
+
+std::string Logger::m_strFileDir  = LOG_FILE_DIR;
+std::string Logger::m_strFileName = LOG_FILE_NAME;
+std::string Logger::m_strFilePath = "";
+bool Logger::m_bSetDir            = false;
+bool Logger::m_bCreateDir         = true;
+
+bool Logger::m_bEnableFileLog     = false;
+int  Logger::m_logFileCount       = 0;
+std::mutex   Logger::m_mutex;
+static Logger* m_pLogger = NULL;
+
+const char* m_logLevelStr[VCA_LOG_LEVEL_COUNT] = {
+    "TRACE",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "FATAL",
+    "NONE"
+};
+
+Logger::Logger()
+{
+}
+
+Logger::~Logger()
+{
+#if LOG_DEBUG_PRNT
+    printf("~Logger m_pLogger==========%p\n", m_pLogger);
+#endif
+    Logger::Dispose();
+    if (m_pLogger != NULL)
+    {
+        delete m_pLogger;
+        m_pLogger = NULL;
+    }
+}
+
+Logger* Logger::Instance()
+{
+#if LOG_DEBUG_PRNT
+    printf("Logger::Instance\n");
+#endif
+    CVcaLockGuard lock(Logger::m_mutex);
+    if (m_pLogger == NULL)
+    {
+        m_pLogger = new(std::nothrow) Logger();
+#if LOG_DEBUG_PRNT
+        printf("new m_pLogger=%p\n", m_pLogger);
+#endif
+    }
+
+    return m_pLogger;
+}
+
+void Logger::SetFileDirectory(
+    IN const std::string &strFileDir,
+    IN bool bDeleteOldDir)
+{
+    CVcaLockGuard lock(Logger::m_mutex);
+    Logger::m_strFileDir = strFileDir;
+    Logger::m_bSetDir = true;
+}
+
+void Logger::SetFileName(const std::string& strFileName)
+{
+    CVcaLockGuard lock(m_mutex);
+    m_strFileName = strFileName;
+    Logger::MakeLogFilePath();
+}
+
+void Logger::SetLevel(IN VCA_LOG_LEVEL logLevel)
+{
+    CVcaLockGuard lock(Logger::m_mutex);
+    Logger::m_logLevel = logLevel;
+}
+
+void Logger::EnableFileLog(IN bool bEnableFileLog)
+{
+    CVcaLockGuard lock(Logger::m_mutex);
+    Logger::m_bEnableFileLog = bEnableFileLog;
+}
+
+void Logger::Log(IN VCA_LOG_LEVEL logLevel, IN const char* const format, ...)
+{
+    CVcaLockGuard lock(Logger::m_mutex);
+
+    int ret = 0;
+
+    if (NULL == m_hLogFile && !Logger::Initialise())
+    {
+#if LOG_DEBUG_PRNT
+        printf("Log return directly!\n");
+#endif
+        return;
+    }
+
+    char szBuffer[MAX_STR_SIZE_INLINE];
+
+    va_list args;
+    va_start(args, format);
+
+    ret = 0;
+    ret = _vsnprintf_s(szBuffer, MAX_STR_SIZE_INLINE-1, format, args);
+    if (ret < 0)
+    {
+#if LOG_DEBUG_PRNT
+        printf("_vsnprintf_s ret = %d!\n", ret);
+#endif
+        return;
+    }
+
+    va_end(args);
+
+#ifdef _WIN32
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    ret = 0;
+    ret = fprintf(m_hLogFile, "[%04u-%02u-%02u %02u:%02u:%02u:%03u] [%s] %s",
+        st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds,
+        m_logLevelStr[logLevel], szBuffer);
+#else if defined __linux__
+    time_t st;
+    struct tm *p = NULL;
+    time(&st);
+    p = localtime(&st);
+    ret = 0;
+    ret = fprintf(m_hLogFile, "[%04u-%02u-%02u %02u:%02u:%02u] [%s] %s",
+        p->tm_year, p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec,
+        m_logLevelStr[logLevel], szBuffer);
+
+    p = NULL;
+#endif
+
+    if (ret < 0)
+    {
+#if LOG_DEBUG_PRNT
+        printf("fprintf ret = %d!\n", ret);
+#endif
+        Logger::Dispose();
+    }
+    else
+    {
+        fflush(m_hLogFile);
+
+        if (ftell(m_hLogFile) >= MAX_LOG_SIZE)
+        {
+            m_logFileCount++;
+            Logger::MakeLogFilePath();
+        }
+    }
+}
+
+bool Logger::Initialise()
+{
+    if (Logger::m_hLogFile != NULL)
+    {
+#if LOG_DEBUG_PRNT
+        printf("Logger::m_hLogFile != NULL -> true\n");
+#endif
+        return true;
+    }
+    if (!Logger::m_bEnableFileLog)
+    {
+#if LOG_DEBUG_PRNT
+        printf("!Logger::m_bEnableFileLog -> false\n");
+#endif
+        return false;
+    }
+
+    if (Logger::m_bCreateDir)
+    {
+        if (!Logger::m_bSetDir)
+        {
+#ifdef __linux__
+            std::string userPth;
+            VcaGetUserPath(userPth);
+            m_strFileDir = userPth + m_strFileDir;
+#if LOG_DEBUG_PRNT
+            printf("m_strFileDir=%s\n", m_strFileDir.c_str());
+#endif
+#endif
+        }
+        if (!CreateFileDirectory(Logger::m_strFileDir))
+        {
+#if LOG_DEBUG_PRNT
+            printf("!CreateFileDirectory(Logger::m_strFileDir) -> false\n");
+#endif
+            return false;
+        }
+        Logger::m_bCreateDir = false;
+    }
+
+#if LOG_DEBUG_PRNT
+    printf("%s\n", Logger::m_strFilePath.c_str());
+#endif
+    Logger::m_hLogFile = fopen(Logger::m_strFilePath.c_str(), "w+");
+    if (Logger::m_hLogFile == NULL)
+    {
+#if LOG_DEBUG_PRNT
+        printf("Logger::m_hLogFile == NULL -> false\n");
+#endif
+        return false;
+    }
+
+    return true;
+}
+
+void Logger::Dispose()
+{
+#if LOG_DEBUG_PRNT
+    printf("Logger::Dispose\n");
+#endif
+    if (NULL != Logger::m_hLogFile)
+    {
+        fflush(Logger::m_hLogFile);
+        fclose(Logger::m_hLogFile);
+        Logger::m_hLogFile = NULL;
+    }
+}
+
+bool Logger::CreateFileDirectory(const std::string& strFileDir, bool bDeleteOldDir)
+{
+    VCA_EXECUTION_STATUS status = VCA_FAILED;
+
+    std::string strDirTemp = strFileDir;
+    if (strDirTemp.substr(strDirTemp.length() - 1, 1) == "/")
+    {
+        strDirTemp = strDirTemp.substr(0, strDirTemp.length() - 1);
+    }
+
+    status = CreateDir(strDirTemp);
+    if (VCA_SUCCESSED != status)
+    {
+#if LOG_DEBUG_PRNT
+        printf("!CreateLogDirectory(strDirTemp) -> false\n");
+#endif
+        return false;
+    }
+
+    if (bDeleteOldDir)
+    {
+        Dispose();
+        status = DeleteDir(Logger::m_strFileDir);
+        if (VCA_SUCCESSED != status)
+        {
+#if LOG_DEBUG_PRNT
+            printf("DeleteDir(m_strFileDir=%s) failed! status=%d\n",
+                Logger::m_strFileDir.c_str(), status);
+#endif
+            return false;
+        }
+    }
+
+    Logger::m_strFileDir = strDirTemp;
+    Logger::MakeLogFilePath();
+
+    return true;
+}
+
+void Logger::MakeLogFilePath()
+{
+    char buf[VCA_PRNT_BUFF_LEN_128] = { 0 };
+
+    Logger::Dispose();
+    Logger::m_strFilePath  = Logger::m_strFileDir + "/";
+    Logger::m_strFilePath += Logger::m_strFileName;
+
+#ifdef _WIN32
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    _snprintf_s(buf, sizeof(buf), "_%4d%02d%02d%02d%02d%02d_%d" LOG_FILE_EXT,
+        st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, Logger::m_logFileCount);
+#else if defined __linux__
+    time_t st(0);
+    struct tm *p = NULL;
+    time(&st);
+    p = localtime(&st);
+    snprintf(buf, sizeof(buf), "_%4d%02d%02d%02d%02d%02d_%d" LOG_FILE_EXT,
+        p->tm_year, p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec, Logger::m_logFileCount);
+
+    p = NULL;
+#endif
+    Logger::m_strFilePath += buf;
+}
---
+++ common/utility/vxlLog.h
@@ -0,0 +1,69 @@
+#ifndef __VXLLOG_H__
+#define __VXLLOG_H__
+
+#include <string>
+#include <mutex>
+
+#include "vcaLogAux.h"
+#include "vcaMutex.h"
+#include "vcaPortableFuncs.h"
+
+#define LogTrace(fmt, ...)   if((Logger::m_logLevel <= VCA_LOG_TRACE) && Logger::m_bEnableFileLog){Logger::Instance()->Log(VCA_LOG_TRACE, fmt, ##__VA_ARGS__);}
+#define LogInfo(fmt, ...)    if((Logger::m_logLevel <= VCA_LOG_INFO) && Logger::m_bEnableFileLog){Logger::Instance()->Log(VCA_LOG_INFO, fmt, ##__VA_ARGS__);}
+#define LogWarning(fmt, ...) if((Logger::m_logLevel <= VCA_LOG_WARNING) && Logger::m_bEnableFileLog){Logger::Instance()->Log(VCA_LOG_WARNING, fmt, ##__VA_ARGS__);}
+#define LogError(fmt, ...)   if((Logger::m_logLevel <= VCA_LOG_ERROR) && Logger::m_bEnableFileLog){Logger::Instance()->Log(VCA_LOG_ERROR, fmt, ##__VA_ARGS__);}
+#define LogFatal(fmt, ...)   if((Logger::m_logLevel <= VCA_LOG_FATAL) && Logger::m_bEnableFileLog){Logger::Instance()->Log(VCA_LOG_FATAL, fmt, ##__VA_ARGS__);}
+#define SetLogFileDirectory  Logger::SetFileDirectory
+#define SetLogFileName       Logger::SetFileName
+#define SetLogLevel          Logger::SetLevel
+#define EnableLogFile        Logger::EnableFileLog
+
+#define VCAEnableLog(logDir, logName, logLevel)                  \
+    if(NULL!=logDir){SetLogFileDirectory(logDir);}               \
+    SetLogFileName(logName);                                     \
+    SetLogLevel(logLevel);                                       \
+    EnableLogFile(true);
+
+#define VCADisableLog()                                          \
+    EnableLogFile(false);
+
+class Logger
+{
+private:
+    Logger();
+public:
+    virtual ~Logger();
+
+    static Logger *Instance();
+    static void SetFileDirectory(IN const std::string &strFileDir, IN bool bDeleteOldDir = false);
+    static void SetFileName(const std::string &strFileName);
+    static void SetLevel(IN VCA_LOG_LEVEL logLevel);
+    static void EnableFileLog(IN bool bEnableFileLog);
+    void Log(IN VCA_LOG_LEVEL logLevel, IN const char *const format, ...);
+
+private:
+    Logger(Logger const &);
+    Logger &operator=(Logger const &);
+
+    static bool Initialise();
+    static void Dispose();
+    static bool CreateFileDirectory(const std::string &strFileDir, bool bDeleteOldDir = false);
+    static void MakeLogFilePath();
+
+    static FILE          *m_hLogFile;
+    static bool          m_bSetDir;
+    static bool          m_bCreateDir;
+
+    static std::string   m_strFileDir;
+    static std::string   m_strFileName;
+    static std::string   m_strFilePath;
+
+    static int           m_logFileCount;
+    static std::mutex    m_mutex;
+
+public:
+    static bool          m_bEnableFileLog;
+    static VCA_LOG_LEVEL m_logLevel;
+};
+
+#endif//__VCALOG_H__
