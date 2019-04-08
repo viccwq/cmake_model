20b90f7bab7fdcda4ca5c7c261d90d3c2b0250c6
viccwq <vic_cwq@163.com>
2019-04-08 23:29:01 +0800

    add verion to lib.dll

--- tool/vxlGetGitInfo.py
+++ tool/vxlGetGitInfo.py
@@ -25,6 +25,26 @@ def replace(old_text, new_text, file_name):
     os.remove(file_name_new)
     return
 
+def parse_version(ver_str):
+    #default version number
+    ver_dst = ['0', '0', '0', '0']
+
+    #split the version string by '.'
+    ver_str  = ver_str.strip()
+    if len(ver_str) > 0:
+        ver_src = ver_str.split('.')
+    else:
+        return ver_dst[0], ver_dst[1], ver_dst[2], ver_dst[3]
+
+    #choose the last 4 version number
+    for i, j in zip(range(len(ver_src)-1, -1, -1), range(len(ver_dst)-1, -1, -1)):
+        print(i, j)
+        ver_src[i] = (re.sub('\D','', ver_src[i]))
+        if len(ver_src[i]) > 0:
+            ver_dst[j] = ver_src[i]
+
+    return ver_dst[0], ver_dst[1], ver_dst[2], ver_dst[3]
+
 if __name__ == '__main__':
     #get info of system 
     if "win32" == sys.platform:
@@ -41,6 +61,20 @@ if __name__ == '__main__':
     if not os.path.exists(git_exe):
         exit("ERROR: can not find git.exe in system!")
 
+    #init variable
+    git_ci_ts   = ""
+    git_cr_time = ""
+    git_ci_time = ""
+    git_version = ""
+    git_author  = ""
+    git_cr_tag  = ""
+
+    ver_status  = "R"
+    ver_major   = "0"
+    ver_minor   = "0"
+    ver_patch   = "0"
+    ver_revision= "0"
+
     #get git information
     if "win32" == sys.platform:
         git_ci_ts   = subprocess.getoutput('"' + git_exe + '"' + ' log -1 --format="%ct"')
@@ -48,13 +82,17 @@ if __name__ == '__main__':
         git_ci_time = subprocess.getoutput('"' + git_exe + '"' + ' log -1 --format="%cd" --date=format:"%Y-%m-%d %H:%M:%S"')
         git_version = subprocess.getoutput('"' + git_exe + '"' + ' log -1 --format="%h"')
         git_author  = subprocess.getoutput('"' + git_exe + '"' + ' log -1 --format="%ae"')
+        git_cr_tag  = subprocess.getoutput('"' + git_exe + '"' + ' tag --points-at ' + git_version)
+        #git_cr_tag  = subprocess.getoutput('"' + git_exe + '"' + ' tag --points-at HEAD^^')
     else:
         git_ci_ts   = ""
         git_cr_time = ""
         git_ci_time = ""
         git_version = ""
         git_author  = ""
-
+        git_cr_tag  = ""
+    
+    (ver_major, ver_minor, ver_patch, ver_revision) = parse_version(git_cr_tag)
     #generate head file
     SRC_FILE_TMP = SRC_FILE+".tmp"
     shutil.copy(SRC_FILE, SRC_FILE_TMP)
@@ -62,6 +100,11 @@ if __name__ == '__main__':
     replace("GIT_AUTHOR",  git_author,  SRC_FILE_TMP)
     replace("GIT_CI_TIME", git_ci_time, SRC_FILE_TMP)
     replace("GIT_CR_TIME", git_cr_time, SRC_FILE_TMP)
+    replace("GIT_CR_TAG",  git_cr_tag,  SRC_FILE_TMP)
+    replace("GIT_VER_STATS", ver_status,  SRC_FILE_TMP)
+    replace("GIT_VER_MAJOR", ver_major,   SRC_FILE_TMP)
+    replace("GIT_VER_MINOR", ver_minor,   SRC_FILE_TMP)
+    replace("GIT_VER_PATCH", ver_patch,   SRC_FILE_TMP)
+    replace("GIT_VER_REVIS", ver_revision,SRC_FILE_TMP)
     shutil.copy(SRC_FILE_TMP, DST_FILE)
     os.remove(SRC_FILE_TMP)
-
